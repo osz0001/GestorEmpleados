aa7a8db95034742d6a3eaede4174768f84c00146
Oscar Sanchez cabeza <osz0001@alu.medac.es>
2024-05-14 12:59:18 +0200

    Creada clase Empleado

---
+++ GestorEmpleados/src/gestorempleados/Empleado.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestorempleados;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Empleado {
+    private String nombre;
+    private double salario;
+    private String departamento;
+
+    public Empleado(String nombre, double salario, String departamento) {
+        this.nombre = nombre;
+        this.salario = salario;
+        this.departamento = departamento;
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "nombre='" + nombre + '\'' +
+                ", salario=" + salario +
+                ", departamento='" + departamento + '\'' +
+                '}';
+    }
+    
+}
